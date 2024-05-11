b6e9327e01e1388ff5032bcece112b966f358e9a
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2024-05-11 23:13:59 +0600

    [authentication] make login and signup page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,20 +1,49 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/Signup/SignUp";
+import Dashboard from "../Layout/Dashboard";
 
 
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children:[
-        {
-            path:'/',
-            element:<Home></Home>
-        }
-      ]
-    },
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      },/*  */
+    ]
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard></Dashboard>,
+    children: [
+      {
+        path: 'employee',
+        element: <Home></Home>
+      },
+      {
+        path: 'teachers',
+        element: <Home></Home>
+      },
+      
+    ]
+  },
+
+
+
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router-dom";
 import { router } from './Routes/Routes';
+import AuthProvider from './Providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='bg-primary'>
   <React.StrictMode>
-  <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+
   </React.StrictMode>,
+  </div>
 )
